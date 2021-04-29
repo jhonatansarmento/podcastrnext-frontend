e8fa6fc3096da3c83c46280f37eaa0258292582f
Jhonatan Sarmento <jhonatan.sarmento@gmail.com>
2021-04-29 18:48:20 -0400

    ending the player component

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -17,7 +17,8 @@ export function Player() {
     episodeList,
     currentEpisodeIndex,
     isPlaying,
-    togglePlay
+    togglePlay,
+    setPlayingState
   } = useContext(PlayerContext)
 
   useEffect(() => {
@@ -80,6 +81,8 @@ export function Player() {
             src={episode.url}
             ref={audioRef}
             autoPlay
+            onPlay={() => setPlayingState(true)}
+            onPause={() => setPlayingState(false)}
           />
         )}
 
